49a5dad31116793f0824dfb4396cf6feedf5ee43
jfiliplic <97060179+jfiliplic@users.noreply.github.com>
2022-12-16 23:05:39 +0100

    added default radio button checked

--- scriptFinal.js
+++ scriptFinal.js
@@ -1,14 +1,12 @@
+radiobutton = document.getElementById("no-gender");
+radiobutton.checked = true;
+
 const formSubmit = document.querySelector('form');
 formSubmit.addEventListener('submit', event => {
   event.preventDefault();
 
   const message = document.getElementById("message").value; 
-  let gender = document.querySelector( 'input[name="Anrede"]:checked');
-  if(gender == null) {
-    gender = "No gender input.";} 
-  else {
-        gender = gender.value;
-  }
+  const gender = document.querySelector( 'input[name="Anrede"]:checked').value;
   const name = document.getElementById("name").value; 
   const email = document.getElementById("email").value; 
   console.log(message+'\n'+gender+'\n'+name+'\n'+email);
